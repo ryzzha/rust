a5439651124bd87243ba831df2cf3cf784aa513d
IraRyzha <ira.ryzha2803@gmail.com>
2025-05-10 20:22:00 +0300

    :)

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+use std::io;
+
+const C: f32 = 32.0;
+
+fn c_to_f(celsius: f32) -> f32 {
+    (celsius * 9.0 / 5.0) + C
+}
+
+fn f_to_c(fahrenheit: f32) -> f32 {
+    (fahrenheit - C) * 5.0 / 9.0
+} 
+
+fn convert(temperature: f32, conversion_type: u8) -> Option<f32> {
+    match conversion_type {
+        1 => Some(c_to_f(temperature)),
+        2 => Some(f_to_c(temperature)),
+        _ => None,
+    }
+}
+
+fn main() { 
+    println!("Select the conversion type: \n 1 -> c_to_f \n 2 -> f_to_c");
+
+    let mut conversion_choice = String::new();
+
+    io::stdin().read_line(&mut conversion_choice).unwrap();
+
+    let conversion_type = conversion_choice.trim().parse::<u8>().expect("Invalid input");
+
+    println!("Enter the temperature to convert:");
+
+    let mut temperature_input = String::new();
+
+    io::stdin().read_line(&mut temperature_input).unwrap();
+
+    let temperature = temperature_input.trim().parse::<f32>().expect("Invalid input");
+
+    let result = convert(temperature, conversion_type);
+
+    match result {
+        Some(result) => println!("The result of conversion is: {result}"),
+        None => println!("Unknown conversion requested!"),
+    };
+
+}
---
+++ src/types.rs
@@ -0,0 +1,26 @@
+fn main() {
+    println!("Hello, world!");
+
+    let mut x: i32 = 11;
+
+    x += 22;
+
+    println!("The value of x is: {}", x);
+
+    // scalar types
+    // int unsigned signed 5 or -5 size bites 8 16 24 32 64 128  2^size
+    // float 5.5
+    // char 'a'
+    // bool true false
+
+    // compound types
+    // turple
+
+    // let base_turple = (1, "ryzha", true);
+    // println!("The value of base_turple is: {}", base_turple.1);
+
+    // array
+
+    // let base_array = [1, 2, 3, 4, 5];
+    // println!("The value of base_array is: {}", base_array[0]);
+}
